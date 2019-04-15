d92f52c6e8f9f623fe6a7dfaec71962d23dd70ea
fenno <fenno.boomgaarden@uni-oldenburg.de>
2019-04-15 02:10:56 +0200

    Sunday Jam results

---
+++ ProjectFiles/Assets/Scripts/LabGenerator.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FieldType
+{
+    FT_StaticWall = 0,
+    FT_DynamicWall,
+    FT_Air
+}
+
+public class LabField
+{
+    public int x = 0, y = 0;
+    public FieldType type = FieldType.FT_Air;
+    public GameObject obj = null;
+
+    public LabField(int x, int y, FieldType type, GameObject obj)
+    {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.obj = obj;
+    }
+}
+
+public class Labyrinth
+{
+    List<List<LabField>> field = null;
+    GameObject wallPrefab;
+    GameObject world;
+
+    public void StartGenerate(int startSizeX, int startSizeY, GameObject world)
+    {
+        this.world = world;
+        wallPrefab = world.GetComponent<LabGenerator>().wallPrefab;
+
+        // Grundwelt erzeugen
+        field = new List<List<LabField>>();
+        for (int x = 0; x < startSizeX; ++x)
+        {
+            field.Add(new List<LabField>());
+            for (int y = 0; y < startSizeY; ++y)
+            {
+                FieldType type = FieldType.FT_DynamicWall;
+
+                if (x % 2 == 1 && y % 2 == 1)
+                {
+                    type = FieldType.FT_StaticWall;
+                }
+                else if (x % 2 == 0 && y % 2 == 0)
+                {
+                    type = FieldType.FT_Air;
+                }
+
+                GameObject obj = null;
+
+                if (type == FieldType.FT_StaticWall || type == FieldType.FT_DynamicWall)
+                {
+                    obj = GameObject.Instantiate(wallPrefab);
+                    obj.transform.SetParent(world.transform);
+                    obj.transform.position = new Vector3(x * 2, 1, y * 2);
+                    obj.SetActive(type == FieldType.FT_StaticWall);
+                }
+
+                field[x].Add(new LabField(x, y, type, obj));
+            }
+        }
+
+        for(int x = 0; x < startSizeX / 20; ++x)
+        {
+            for(int y = 0; y < startSizeY / 20; ++y)
+            {
+                GenerateChunk(x * 20, y * 20, 20);
+            }
+        }
+    }
+        
+    bool GetChunkState(int chunkX, int chunkY, int chunkSize)
+    {
+        // Enthält false, falls ein abgetrennter Raum gefunden wurde
+        bool result = true;
+
+        List<List<int>> bufferstate = new List<List<int>>();
+
+        for(int x = 0; x < chunkSize / 2; ++x)
+        {
+            bufferstate.Add(new List<int>());
+            for(int y = 0; y < chunkSize / 2; ++y)
+            {
+                // Alles kommt erstmal auf 10000
+                bufferstate[x].Add(10000);
+
+                // Äußer die Eingänge
+                if(x == chunkSize / 4 && y == chunkSize / 4)
+                {
+                    bufferstate[x][y] = 0;
+                }
+                
+            }
+        }
+        
+        // Oft genug iterieren
+        for(int i = 0; i < chunkSize / 2 * 5; ++i)
+        {
+            // Wir nehmen zu Beginn der Iteration einen fehlerlosen Zustand an und suchen dann nach Fehlern
+            result = true;
+
+            for (int x = 0; x < chunkSize / 2; ++x)
+            {
+                for (int y = 0; y < chunkSize / 2; ++y)
+                {
+                    if (bufferstate[x][y] == 10000)
+                    {
+                        result = false;
+                    }
+
+                    if (x > 0 && bufferstate[x - 1][y] + 1 < bufferstate[x][y])
+                    {
+                        // Links
+                        if (!field[chunkX + 2 * x - 1][chunkY + 2 * y].obj.activeSelf)
+                        {
+                            bufferstate[x][y] = bufferstate[x - 1][y] + 1;
+                        }
+                    }
+                    
+                    if (y > 0 && bufferstate[x][y - 1] + 1 < bufferstate[x][y])
+                    {
+                        // Hinten
+                        if (!field[chunkX + 2 * x][chunkY + 2 * y - 1].obj.activeSelf)
+                        {
+                            bufferstate[x][y] = bufferstate[x][y - 1] + 1;
+                        }
+                    }
+
+                    if (x < chunkSize / 2 - 1 && bufferstate[x + 1][y] + 1 < bufferstate[x][y])
+                    {
+                        // Rechts
+                        if (!field[chunkX + 2 * x + 1][chunkY + 2 * y].obj.activeSelf)
+                        {
+                            bufferstate[x][y] = bufferstate[x + 1][y] + 1;
+                        }
+                    }
+
+                    if (y < chunkSize / 2 - 1 && bufferstate[x][y + 1] + 1 < bufferstate[x][y])
+                    {
+                        // Vorne
+                        if (!field[chunkX + 2 * x][chunkY + 2 * y + 1].obj.activeSelf)
+                        {
+                            bufferstate[x][y] = bufferstate[x][y + 1] + 1;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    void GenerateChunk(int chunkX, int chunkY, int chunkSize)
+    {
+        GameObject last = null;
+        
+        int counter = 0;
+
+        while(counter < 100)
+        {
+            int x = Random.Range(0, chunkSize / 2) * 2;
+            int y = Random.Range(0, chunkSize / 2) * 2;
+
+            if(Random.Range(0f, 1f) > 0.5f)
+            {
+                x += 1;
+            }
+            else
+            {
+                y += 1;
+            }
+
+            last = field[chunkX + x][chunkY + y].obj;
+            last.SetActive(true);
+
+            if (!GetChunkState(chunkX, chunkY, chunkSize))
+            {
+                counter++;
+                last.SetActive(false);
+            }
+        }
+
+        last.SetActive(false);
+
+        //for (int x = 0; x < chunkSize / 2; ++x)
+        //{
+        //    for (int y = 0; y < chunkSize / 2; ++y)
+        //    {
+        //        last = field[chunkX + x * 2 + 1][chunkY + y * 2].obj;
+        //        last.SetActive(true);
+        //        if (!GetChunkState(chunkX, chunkY, chunkSize))
+        //        {
+        //            last.SetActive(false);
+        //        }
+
+        //        last = field[chunkX + x * 2][chunkY + y * 2 + 1].obj;
+        //        last.SetActive(true);
+        //        if (!GetChunkState(chunkX, chunkY, chunkSize))
+        //        {
+        //            last.SetActive(false);
+        //        }
+        //    }
+        //}
+    }
+}
+
+public class LabGenerator : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public Labyrinth lab;
+
+    // Start the generation
+    void Start()
+    {
+        lab = new Labyrinth();
+        lab.StartGenerate(100, 100, gameObject);
+    }
+    
+}
---
+++ ProjectFiles/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    public float mouseSensitivity = 2.5f;
+
+    private Vector3 moveDirection = new Vector3(1, 0, 0);
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Vector3 change = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")); ;
+
+        change *= mouseSensitivity;
+
+        float x = transform.rotation.eulerAngles.x;
+        float y = transform.rotation.eulerAngles.y;
+
+        x -= change.y;
+
+        y += change.x;
+
+        transform.rotation = Quaternion.Euler(new Vector3(x, y));
+
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(transform.TransformDirection(moveDirection).x, 0f, transform.TransformDirection(moveDirection).z);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    CharacterController c = GetComponent<CharacterController>();
+        
+    //    if(Input.GetKeyDown(KeyCode.W))
+    //    {
+    //        c.Move(Vector3)
+    //    }
+    //    if(Input.GetKeyDown(KeyCode.A))
+    //    {
+
+    //    }
+    //    if(Input.GetKeyDown(KeyCode.S))
+    //    {
+
+    //    }
+    //    if(Input.GetKeyDown(KeyCode.D))
+    //    {
+
+    //    }
+    //}
+}
